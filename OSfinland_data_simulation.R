187e145c85a0424085ed058b42f2a22f7674ca54
pzturune <pzturune@users.noreply.github.com>
2021-03-02 13:07:29 +0200

    Update OSfinland_data_simulation.R
    Changes made to reflect the final state of the questionnaire; not finished yet!

--- OSfinland_data_simulation.R
+++ OSfinland_data_simulation.R
@@ -4,6 +4,11 @@
 
 ### PRE DATA-COLLECTION DATA SIMULATION
 
+### Original code by Anton Kunnari
+### Updated to reflect the final questionnaire by Piia Turunen
+
+###please note that some responses allow for multiple choice but are not coded here thusly, e.g. Q3&Q7
+
 set.seed(0)
 
 # sample size
@@ -11,36 +16,26 @@ n = 100
 
 dat <- data.frame(ID = 1:n)
 
-labs <- c("Doctoral researcher, PhD/Doctoral student/candidate", 
-          "Postdoctoral researcher",
-          "Lecturer",
-          "Assistant professor",
-          "Professor or principal investigator",
-          "I’m not currently doing research (in addition, please select another option describing your latest position as a researcher)",
-          "Other, please describe")
+labs <- c("First stage", 
+          "Second stage",
+          "Third stage",
+          "Fourth stage")
 
 dat$position <- sample(labs, n, replace=TRUE)
 
-labs <- c("Qualitative research", "Quantitative research", "Other, please describe")
-
-dat$res_area <-  sample(labs, n, replace=TRUE)
-
-paper_num <- round(rexp(n, rate=1.5)*10)
-
-paper_num
-
-labs <- c("Political science",
-          "Psychology",
-          "Economics",
-          "Social sciences",
-          "Humanities",
-          "Population or health sciences",
-          "Linguistics",
-          "Other, please describe")
+labs <- c("Psychology",
+          "Social psychology",
+          "Other")
 
 dat$field <- sample(labs, n, replace=TRUE)
 
-## 5. How much experience do you have of the following open science practices?
+labs <- c("Qualitative research", "Quantitative research", "Other")
+
+dat$res_area <-  sample(labs, n, replace=TRUE)
+
+
+
+## 4. How much experience do you have of the following open science practices?
   
 ## How much experience do you have of the following open science practices?
 ## Item anchoring:
@@ -59,7 +54,7 @@ dat$OSexp.regrep <- rbinom(100, 4, 0.5)+1
 dat$OSexp.preprint <- rbinom(100, 4, 0.5)+1
 dat$OSexp.open.edu <- rbinom(100, 4, 0.5)+1
 
-## 6. How important do you think the following open science practices are?
+## 5. How important do you think the following open science practices are?
 ## Item anchoring:
 # 1. Completely unimportant
 # 2. Somewhat unimportant
@@ -76,7 +71,7 @@ dat$OSimp.regrep <- rbinom(100, 4, 0.5)+1
 dat$OSimp.preprint <- rbinom(100, 4, 0.5)+1
 dat$OSimp.open.edu <- rbinom(100, 4, 0.5)+1
 
-##7. What kind of guidance would you like to receive for the following?
+##6. What kind of guidance would you like to receive for the following?
 
 labs <- c("Written material", 
           "Lectures", 
@@ -94,51 +89,50 @@ colnames(mat) <- nams
 
 dat <- cbind(dat, mat)
 
-labs <- c(
-"I learned about the practice too late to implement it",
-"My workload was already too heavy",
-"Implementing open science practices took too much time",
-"I didn’t have enough funding to implement open science practices",
-"Carrying out the open practice was too difficult",
-"I didn’t receive sufficient information or guidance for carrying out the practice",
+## 7. Which language would you like the materials or training to be in? 
+##    You can choose multiple option if you wish.
+# please not: in this code, each respondent only chooses one option unlike in the questionnaire
 
-"Paper was rejected in peer review because of the use of open science practices",
-"Implementing open science practices would have decreased publishing opportunities",
+labs <- c("Finnish", 
+          "English",
+          "Swedish",
+          "other")
+
+dat$language <- sample(labs, n, replace=TRUE)
+
+
+## 8. Which of the following obstacles to implementing open research practices have you experienced during the past two years? Choose all that apply.
+
+
+
+labs <- c(
+"I didn’t think open science practices were necessary or important",
+"Open science practices were not applicable to my research",
+"I was not familiar with open science practices",
+"I lacked the necessary knowledge",
+"I did not receive guidance",
+
+"The open practices were too difficult to implement",
+"My workload was already too heavy",
+"Implementing open science practices would have taken too much time",
+"I did not have sufficient funding",
+"Implementing open science practices could have decreased publishing opportunities",
+
 "Other researchers could have misused information, data, or analysis code",
 "My research idea could have been stolen",
 "My study would have been more exposed to criticism",
-
-"Participants of the study were not asked for permission for data sharing",
 "Sensitivity of the data or other confidentiality related issues prevented implementing the practice",
 "There wasn’t a suitable platform or archive for openly sharing the data",
 
-"Faculty or other work community hadmost a negative attitude towards open practices",
+"There was no permission to share the data from the participants of the study",
+"Department or other work community had a negative attitude towards open practices",
 "Employer or boss had a negative attitude towards open practices",
 "Reviewers recommended removing or altering the used open science practices",
 "A third party prevented implementing the practices",
-"Business related reasons prevented implementing the practices",
+
 "I wasn’t motivated because others don’t implement open science practices",
-"I didn’t think open science practices were necessary or important")
-
-mat <- matrix(round(runif(length(labs)*n, 0, 1)), nrow=n, ncol=length(labs))
-colnames(mat) <- labs
-
-dat <- cbind(dat, mat)
-
-dat$other.obstacles <- rep("Lorem Ipsum", n)
-
-labs <- c("Journal or funder requires open research practices from all",
-"Open research practices are the norm in my science community",
-"My employer requires open research practices",
-"Part of the working time or funding is reserved for learning about and implementing open practices",
-"Training and guidance for implementing open research practices",
-"Implementing open research practices advances my career as a researcher (e.g. it’s considered in evaluations)",
-"Implementing open research practices enhances my future funding possibilities",
-"Implementing open research practices leads to a higher citation count",
-"Implementing open research practices leads to a better reputation as a researcher",
-"Implementing open research practices increases the probability of getting published",
-"Nothing",
-"Other, please describe")
+"Other, please specify:",
+"I have not experienced any of the obstacles mentioned above")
 
 mat <- matrix(0, nrow=n, ncol=length(labs))
 for(row in 1:n) {
@@ -148,6 +142,31 @@ for(row in 1:n) {
 colnames(mat) <- labs
 dat <- cbind(dat, mat)
 
+## 10. I would implement open practices more if...(choose up to 5 most significant)
+# please not: responses are not restricted to 5 in this code
+
+labs <- c(
+  "Journal or funder would open research practices from all",
+  "Open research practices were the norm in my science community",
+  "My employer would require open research practices",
+  "Part of the working time or funding would be reserved for learning about and implementing open practices",
+  "Training and guidance for implementing open research practices would be available",
+  
+  "Implementing open research practices would advance my career as a researcher (e.g. considered in evaluations)",
+  "Implementing open research practices would enhance my future funding possibilities",
+  "Implementing open research practices would lead to a higher citation count",
+  "Implementing open research practices would lead to a better reputation as a researcher",
+  "Implementing open research practices would increase the probability of getting published",
+  
+  "Other, please describe")
+
+mat <- matrix(0, nrow=n, ncol=length(labs))
+for(row in 1:n) {
+  mat[row,sample(1:12, 5)] <- 1
+}
+
+colnames(mat) <- labs
+dat <- cbind(dat, mat)
 
 ## 11. Are you familiar with the term replication crisis?
 labs <- c("I don’t recall having heard of it",
